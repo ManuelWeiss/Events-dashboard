011e5b185c2c2397a7afab373f19c07a4ad9ee4d
Claudia Matosa <cmatosa@moonfruit.com>
2014-07-03 17:26:32 +0100

    Fixing compilation error

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-      "org.webjars" %% "webjars-play" % "2.2.2-1",
+      "org.webjars" %% "webjars-play" % "2.2.2-1"
       )
 
   val main = play.Project(appName, appVersion, appDependencies).settings()
