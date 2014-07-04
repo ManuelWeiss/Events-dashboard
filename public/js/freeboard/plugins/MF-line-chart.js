a6db03e5da3ef1962950181e98cd8ead13b5db26
Claudia Matosa <cmatosa@moonfruit.com>
2014-07-04 14:48:39 +0100

    Using real events

--- public/js/freeboard/plugins/MF-line-chart.js
+++ public/js/freeboard/plugins/MF-line-chart.js
@@ -26,29 +26,29 @@
                 type: 'spline',
                 animation: Highcharts.svg, // don't animate in old IE
                 marginRight: 10,
-                height: 180,
+                height: 150,
                 backgroundColor: 'none',
                 events: {
                     load: function() {
                         // set up the updating of the chart each second
                         var series = this.series[0];
-                        setInterval(function() {
+
+                        $(element).on('newValues', function (event, value) {
                             var x = (new Date()).getTime(), // current time
-                                y = Math.random();
+                                y = value;
                             series.addPoint([x, y], true, true);
-                        }, 1000);
+                        });
                     }
                 }
             },
             colors: ["#FF9500"],
             xAxis: {
                 type: 'datetime',
-                tickPixelInterval: 150,
-                lineColor: '#B88F51'
+                tickPixelInterval: 150
             },
             yAxis: {
                 title: {
-                    text: 'Number of registrations'
+                    text: 'Amount'
                 },
                 plotLines: [{
                     value: 0,
@@ -70,7 +70,8 @@
                 enabled: false
             },
             series: [{
-                name: 'Random data',
+                name: 'series-1',
+                id: 'event1',
                 data: (function() {
                     // generate an array of random data
                     var data = [],
@@ -80,25 +81,41 @@
                     for (i = -19; i <= 0; i++) {
                         data.push({
                             x: time + i * 1000,
-                            y: Math.random()
+                            y: 0
                         });
                     }
+
                     return data;
                 })()
             }]
         });
     };
 
+    var addValueToChart = function (element, eventAmount) {
+        $(element).trigger('newValues', [eventAmount]);
+    };
+
     var lineChartWidget = function (settings) {
         var self = this;
+        var eventCount = 0;
+        var thisChart;
 
         var titleElement = $('<h2 class="linechart-title section-title"></h2>');
         var lineChartElement = $('<div class="linechart" style="width: 100%, height: 100%"></div>');
 
+        var updateTimer = function () {
+            setInterval($.proxy(function () {
+                addValueToChart(thisChart, eventCount);
+                eventCount = 0;
+            }, this), 1000);
+        };
+
         this.render = function (element) {
             $(element).append(titleElement).append(lineChartElement);
 
+            thisChart = element;
             renderLineChart(element);
+            updateTimer();
         }
 
         this.onSettingsChanged = function (newSettings) {
@@ -106,14 +123,14 @@
         }
 
         this.onCalculatedValueChanged = function (settingName, newValue) {
-            // addValueToSparkline(lineChartElement, newValue);
+            eventCount += 1;
         }
 
         this.onDispose = function () {
         }
 
         this.getHeight = function () {
-            return 3.5;
+            return 3;
         }
 
         this.onSettingsChanged(settings);
